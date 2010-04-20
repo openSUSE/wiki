f14e55583706e0d0c482702b1d3e51ff792e5e66
digitaltom <digitaltom@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2010-04-20 10:48:51 +0000

    no newlines in js code

--- skins/OpenSUSE.php
+++ skins/OpenSUSE.php
@@ -369,10 +369,8 @@ var $skin;
   <?php $this->html('reporttime') ?>
   <script type="text/javascript">
    //<![CDATA[
-   var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl
-   ." : "http://www.");
-   document.write(unescape("%3Cscript src='" + gaJsHost + "google- 
-   analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+   var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+   document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
    //]]>
   </script>
   <script type="text/javascript">
