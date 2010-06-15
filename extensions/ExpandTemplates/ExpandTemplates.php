76efae41a2b75f6b89448abbd65af15c5df04a44
digitaltom <digitaltom@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2010-06-15 15:16:11 +0000

    add extensions

---
+++ extensions/ExpandTemplates/ExpandTemplates.php
@@ -0,0 +1,26 @@
+<?php
+if ( !defined( 'MEDIAWIKI' ) ) {
+?>
+<p>This is the ExpandTemplates extension. To enable it, put </p>
+<pre>require_once("$IP/extensions/ExpandTemplates/ExpandTemplates.php");</pre>
+<p>at the bottom of your LocalSettings.php.</p>
+<?php
+	exit(1);
+}
+
+$wgExtensionCredits['specialpage'][] = array(
+	'name'           => 'ExpandTemplates',
+	'svn-date' => '$LastChangedDate: 2008-08-11 18:19:14 +0000 (Mon, 11 Aug 2008) $',
+	'svn-revision' => '$LastChangedRevision: 39164 $',
+	'author'         => 'Tim Starling',
+	'url'            => 'http://www.mediawiki.org/wiki/Extension:ExpandTemplates',
+	'description'    => 'Expands templates, parser functions and variables to show expanded wikitext and preview rendered page',
+	'descriptionmsg' => 'expandtemplates-desc',
+);
+
+$dir = dirname(__FILE__) . '/';
+$wgAutoloadClasses['ExpandTemplates'] = $dir . 'ExpandTemplates_body.php';
+$wgExtensionMessagesFiles['ExpandTemplates'] = $dir . 'ExpandTemplates.i18n.php';
+$wgExtensionAliasesFiles['ExpandTemplates'] = $dir . 'ExpandTemplates.alias.php';
+$wgSpecialPages['ExpandTemplates'] = 'ExpandTemplates';
+$wgSpecialPageGroups['ExpandTemplates'] = 'wiki';
