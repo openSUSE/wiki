0b160b375b7a8c5182501937d1d04344e7936f91
digitaltom <digitaltom@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2010-10-26 12:08:53 +0000

    fix zh_TW lang

--- extensions/BentoLanguage.php
+++ extensions/BentoLanguage.php
@@ -2,7 +2,7 @@
 
 global $bento_lang;
 
-$avail_bento_langs = array("cs", "de", "el", "en", "es", "fi", "fr", "hu", "it", "ja", "nl", "pl", "pt", "ru", "sv", "tr", "vi", "zh_tw");
+$avail_bento_langs = array("cs", "de", "el", "en", "es", "fi", "fr", "hu", "it", "ja", "nl", "pl", "pt", "ru", "sv", "tr", "vi", "zh_TW");
 $bento_lang = "en";
 $wiki_lang = substr($_SERVER['SERVER_NAME'], 0, strpos($_SERVER['SERVER_NAME'], '.') );
 $wiki_lang = str_replace( 'stage', '', $wiki_lang );
