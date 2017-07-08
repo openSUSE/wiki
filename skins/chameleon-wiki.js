94ea1735f475fe25bbad6d050987147dca623cbc
Guo Yunhe <guoyunhebrave@gmail.com>
2017-07-08 15:50:45 +0300

    Responsive images and tables

--- skins/chameleon-wiki.js
+++ skins/chameleon-wiki.js
@@ -3,3 +3,10 @@ $('.card img').addClass('card-img-top img-fluid');
 
 // .mw-editsection
 $('.mw-editsection a').addClass('btn btn-secondary btn-sm');
+
+// images
+$('#bodyContent img').addClass('img-fluid');
+$('#bodyContent .thumbinner').css('width', 'auto');
+
+// table
+$('#bodyContent table').addClass('table').addClass('table-responsive');
