b9530d74e2f80e2471194a55216fefeec1821113
Guo Yunhe <guoyunhebrave@gmail.com>
2017-07-20 00:40:40 +0300

    Migrate Chameleon skin to MediaWiki 1.27

--- skins/Chameleon/chameleon-wiki.js
+++
@@ -1,8 +0,0 @@
-// .card-image
-$('.card img').addClass('card-img-top img-fluid');
-
-// .mw-editsection
-$('.mw-editsection a').addClass('btn btn-secondary btn-sm');
-
-// table
-$('#bodyContent table').addClass('table').addClass('table-responsive');
---
+++ skins/Chameleon/wiki.js
@@ -0,0 +1,8 @@
+// .card-image
+jQuery('.card img').addClass('card-img-top img-fluid');
+
+// .mw-editsection
+jQuery('.mw-editsection a').addClass('btn btn-secondary btn-sm');
+
+// table
+jQuery('#bodyContent table').addClass('table').addClass('table-responsive');
