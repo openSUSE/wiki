ad43286c42eff4b83f83e513ee311a060777064b
Guo Yunhe <guoyunhebrave@gmail.com>
2017-07-20 00:52:32 +0300

    Add Piwik script

--- skins/Chameleon/SkinChameleon.php
+++ skins/Chameleon/SkinChameleon.php
@@ -457,6 +457,25 @@ class ChameleonTemplate extends BaseTemplate
 
 <?php $this->printTrail(); ?>
 
+<script>
+var _paq = _paq || [];
+(function () {
+    var u = (("https:" == document.location.protocol) ? "https://beans.opensuse.org/piwik/" : "http://beans.opensuse.org/piwik/");
+    _paq.push(['setSiteId', 9]);
+    _paq.push(['setTrackerUrl', u + 'piwik.php']);
+    _paq.push(['trackPageView']);
+    _paq.push(['setDomains', ["*.opensuse.org"]]);
+    var d = document,
+        g = d.createElement('script'),
+        s = d.getElementsByTagName('script')[0];
+    g.type = 'text/javascript';
+    g.defer = true;
+    g.async = true;
+    g.src = u + 'piwik.js';
+    s.parentNode.insertBefore(g, s);
+})();
+</script>
+
 </body>
 </html>
 <?php
