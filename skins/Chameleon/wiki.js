722141cf335407c5ca61c4d78dbfdcf098fa791d
Guo Yunhe <guoyunhebrave@gmail.com>
2017-08-13 17:16:54 +0300

    Remove default MediaWiki classes

--- skins/Chameleon/wiki.js
+++ skins/Chameleon/wiki.js
@@ -6,3 +6,6 @@ jQuery('.mw-editsection a').addClass('btn btn-secondary btn-sm');
 
 // table
 jQuery('#bodyContent table').addClass('table').addClass('table-responsive');
+
+// Remove some MediaWiki classes that override skin styles
+jQuery('.mw-content-ltr').removeClass('mw-content-ltr');
