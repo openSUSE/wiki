8a9f4bd4efdc96ffe839e2cda97e4d9c441df084
Guo Yunhe <guoyunhebrave@gmail.com>
2017-08-13 17:31:32 +0300

    Improve card styles with JavaScript helper

--- skins/Chameleon/wiki.js
+++ skins/Chameleon/wiki.js
@@ -1,5 +1,13 @@
+// .card unnecessary <p> tag by MediaWiki
+jQuery('.card > p, .card-block > p:not(.card-title):not(.card-text)').each(function () {
+    jQuery(this).after(jQuery(this).html());
+    jQuery(this).remove();
+});
+
 // .card-image
 jQuery('.card img').addClass('card-img-top img-fluid');
+// .card-link
+jQuery('.card .card-block')
 
 // .mw-editsection
 jQuery('.mw-editsection a').addClass('btn btn-secondary btn-sm');
