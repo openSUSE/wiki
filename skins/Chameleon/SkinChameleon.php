6a014f082f3d729ac7981fe932d31b119e0070de
Guo Yunhe <guoyunhebrave@gmail.com>
2017-12-23 13:41:05 +0200

    Migrate navbar

--- skins/Chameleon/SkinChameleon.php
+++ skins/Chameleon/SkinChameleon.php
@@ -114,15 +114,14 @@ class ChameleonTemplate extends BaseTemplate
 ?>
 
 <!-- Global Navbar -->
-<nav id="global-navbar" class="navbar navbar-expand-md navbar-dark bg-dark">
+<nav id="global-navbar" class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <a class="navbar-brand" href="https://www.opensuse.org/">
+        <img src="<?= $wgStylePath ?>/Chameleon/res/images/logo/logo-white.svg" width="48" height="30" class="d-inline-block align-top" alt="openSUSE Logo">
+    </a>
 
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-
-    <a class="navbar-brand" href="https://www.opensuse.org/">
-        <img src="<?= $wgStylePath ?>/Chameleon/res/images/logo/logo-white.svg" width="48" height="30" class="d-inline-block align-top" alt="openSUSE Logo">
-    </a>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
