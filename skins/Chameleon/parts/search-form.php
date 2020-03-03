f5b9fc4f306a4927ad1f58e8f4f19131251b0d48
Guo Yunhe <i@guoyunhe.me>
2020-03-03 23:22:21 +0200

    improve navbar and footer styles

--- skins/Chameleon/parts/search-form.php
+++ skins/Chameleon/parts/search-form.php
@@ -1,3 +1,12 @@
 <form action="<?php $this->text( 'wgScript' ) ?>" id="searchform" class="form-inline">
-	<?php echo $this->makeSearchInput( array( 'id' => 'searchInput', 'class' => 'form-control', 'type' => 'search' ) ); ?>
+	<div class="input-group">
+		<?php echo $this->makeSearchInput( array( 'id' => 'searchInput', 'class' => 'form-control', 'type' => 'search' ) ); ?>
+		<div class="input-group-append">
+			<button class="btn btn-secondary" type="submit">
+				<svg class="icon">
+					<use xlink:href="#search-line">
+				</svg>
+			</button>
+		</div>
+	</div>
 </form>
