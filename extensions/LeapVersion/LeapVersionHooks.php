7b314d26b81fb9db48e8e8f6b966b9a1a3a84946
Christian Boltz <opensuse@cboltz.de>
2021-12-17 23:07:15 +0100

    update LeapVersion extension to 15.3

--- extensions/LeapVersion/LeapVersionHooks.php
+++ extensions/LeapVersion/LeapVersionHooks.php
@@ -4,9 +4,9 @@ class LeapVersionHooks {
 	public static function onParserBeforeInternalParse( &$parser, &$text, &$strip_state )
 	{
 		$versions = [
-			'LEAP_VERSION' => '15.2',
-			'LEAP_VERSION_OLD' => '15.1',
-			'LEAP_VERSION_OLDER' => '15.0',
+			'LEAP_VERSION' => '15.3',
+			'LEAP_VERSION_OLD' => '15.2',
+			'LEAP_VERSION_OLDER' => '15.1',
 		];
 
 		foreach ($versions as $key => $value) {
